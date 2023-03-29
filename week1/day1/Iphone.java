d0fe9e3b9124d4240d5c9ef7301a69d676a96293
Balaji ts <64155315+BALAJITATHAN@users.noreply.github.com>
2023-03-29 22:34:39 +0530

    Add files via upload
    Week1-D1

---
+++ week1/day1/Iphone.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Iphone {
+public void makeCall() 
+{
+	System.out.println("make call to him");
+}
+public void model()
+{
+	System.out.println("model is");
+}
+
+}
---
+++ week1/day1/Mobile.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Mobile {
+public void sendSms(){
+	System.out.println("From oppo");
+};
+public void makeCall()
+{
+	System.out.println("Calling my friend");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Mobile phone=new Mobile();
+phone.sendSms();
+phone.makeCall(); 
+	}
+
+}
+ 
